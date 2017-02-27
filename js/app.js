be0eb8bf145d6dc7982ca0e2fedbad96da5bc947
namelessprofit <posadaarchy@gmail.com>
2017-02-27 00:22:10 -0800

    add logic for draw, and winner on top row, did not finish adding arguments to declcare winner on the rest of the winning combinations.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 // wait for the DOM to finish loading
 $(document).ready(function() {
 
-var board =[[],[],[]]
-var state = false
+var board =[[],[],[]];
+var state = false;
 var counter = 0;
+
 
 function populateBoard(el,i){ // helper function for filtering
   if(el.hasChildNodes()){
@@ -17,17 +18,41 @@ function populateBoard(el,i){ // helper function for filtering
    }
 }
 
-function filterX(x){
+function winner(arr){ //checks arr for winning solutions
+  for (var i = 0; i< arr.length; i++){
+    for ( var x = 0; x < arr[i].length; x++){
+      if(arr[0][0].childNodes[0].data == "X" && arr[0][1].childNodes[0].data == "X" && arr[0][2].childNodes[0].data == "X"){
+        alert('X is the winner!')
+        return false;
+      }
+    }
+  }
+}
+
+function filterX(x){ //filtering out the positions of x inside board
  x.forEach(function(e,i){
-  console.log(e[i].childNodes[0].data)
-})
+  if (e.childNodes[0].data == 'X') {
+  return e.childNodes[0].data
+    }
+  })
+}
+
+function filterO(o){ //filtering out the positions of o inside board
+ o.forEach(function(e,i){
+  if (e.childNodes[0].data == 'O') {
+  return e.childNodes[0].data
+    }
+  })
 }
 
 function checker(){ //checks for winner
   board =[[],[],[]];
   var arr = $('.box').toArray();
   arr.forEach(populateBoard);
-  board.forEach(filterX)
+  winner(board);
+  // var x = board.filter(filterX);
+  // var o = board.filter(filterO);
+// console.log(x);
 }
 
 $('.box').on('click',function(){
@@ -38,15 +63,20 @@ $('.box').on('click',function(){
         $(this).text("O");
     }else{
         $(this).text("X");
-      this.symbol = true;
     }
     state = !state; //toggle between states
     counter++
     if(counter >= 5){ //runing array checker after 5 clicks
       checker();
+      if (counter >= 9){
+        alert("Draw!");
+      }
     }
   }
 
+
 });
-$('#reload').click(function() { location.reload(); })
+$('#reload').click(function() {
+  location.reload();
+ })
 });
