696795cb4526ed735c437ec56de48f7ff4d3ffb8
namelessprofit <posadaarchy@gmail.com>
2017-02-26 21:03:57 -0800

    lost all my commits due to incorrect repo. forked new repo and replaced files. work being uploaded now contain logic to reset board, alternate player turns, mark each box with X or O, prevents taken box from being marked if the box is already used. marked boxes return values which ewil be used to create an array and check for winners..

--- js/app.js
+++ js/app.js
@@ -1,6 +1,52 @@
 // wait for the DOM to finish loading
 $(document).ready(function() {
-  // all code to manipulate the DOM
-  // goes inside this function
 
+var board =[[],[],[]]
+var state = false
+var counter = 0;
+
+function populateBoard(el,i){ // helper function for filtering
+  if(el.hasChildNodes()){
+      if(i == 0 || i == 1 || i == 2){
+        board[0].push(el);
+      } else if (i == 3 || i == 4 || i == 5){
+        board[1].push(el);
+      }else if (i == 6 || i == 7 || i == 8){
+        board[2].push(el);
+      }
+   }
+}
+
+function filterX(x){
+ x.forEach(function(e,i){
+  console.log(e[i].childNodes[0].data)
+})
+}
+
+function checker(){ //checks for winner
+  board =[[],[],[]];
+  var arr = $('.box').toArray();
+  arr.forEach(populateBoard);
+  board.forEach(filterX)
+}
+
+$('.box').on('click',function(){
+  if(this.hasChildNodes()){ //deny change if symbol exists
+    return false;
+  }else{
+    if(state){ //checking for state (X or O)
+        $(this).text("O");
+    }else{
+        $(this).text("X");
+      this.symbol = true;
+    }
+    state = !state; //toggle between states
+    counter++
+    if(counter >= 5){ //runing array checker after 5 clicks
+      checker();
+    }
+  }
+
+});
+$('#reload').click(function() { location.reload(); })
 });
